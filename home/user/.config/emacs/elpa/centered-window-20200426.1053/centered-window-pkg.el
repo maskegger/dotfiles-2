6d64331775101f849f133973d6a5a23f680a8c23
MCotocel <mcotocel@outlook.com>
2021-04-23 09:01:23 +0400

    Removed stuff

--- home/user/.config/emacs/elpa/centered-window-20200426.1053/centered-window-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from centered-window.el  -*- no-byte-compile: t -*-
-(define-package "centered-window" "20200426.1053" "Center the text when there's only one window" '((emacs "24.4")) :commit "f50859941ab5c7cbeaee410f2d38716252b552ac" :authors '(("Anler Hernández Peral" . "inbox+emacs@anler.me")) :maintainer '("Anler Hernández Peral" . "inbox+emacs@anler.me") :keywords '("faces" "windows") :url "https://github.com/anler/centered-window-mode")
