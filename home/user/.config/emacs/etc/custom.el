ea219389546284a39c7f75985e16d058bf69aaea
MCotocel <MCotocel@outlook.com>
2021-06-14 20:52:12 +0400

    Removed cache files, updated emacs config

--- home/user/.config/emacs/etc/custom.el
+++ home/user/.config/emacs/etc/custom.el
@@ -9,5 +9,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(json-mode writeroom-mode which-key vterm use-package undo-tree smartparens rainbow-delimiters org-roam-server org-bullets neotree multiple-cursors md4rd magit lua-mode lsp-ui lsp-jedi helm-lsp helm-flx good-scroll format-all evil-leader erc-image erc-hl-nicks doom-themes doom-modeline dashboard dap-mode company-box centaur-tabs atom-one-dark-theme)))
+ '(custom-safe-themes
+   '("88f59acbeacefb4998f45126d4d8ae8b2184f2a48753db362a349fd55321c7e1" "6c386d159853b0ee6695b45e64f598ed45bd67c47f671f69100817d7db64724d" "6b1abd26f3e38be1823bd151a96117b288062c6cde5253823539c6926c3bb178" "7a7b1d475b42c1a0b61f3b1d1225dd249ffa1abb1b7f726aec59ac7ca3bf4dae" "13a80aeec9b23390827821a3307bc7e6b2e1ca49acbd8a76e628316d4f91d33d" "e7ab6199e2227032ae8722423329630cfb7a5fdd54ee0f3953afc9a4e8d47727" "31f19c51820b3f91e6b1698050b5cb9f8b18e9d40e7bb789ad39be941e41cbe4" "95690b4048476956a6c6c1ab239df31f2d97e6ee41dbec251d9725b7df70c14b" "7661b762556018a44a29477b84757994d8386d6edee909409fabe0631952dad9" "6b5c518d1c250a8ce17463b7e435e9e20faa84f3f7defba8b579d4f5925f60c1" default)))
---
+++ home/user/.config/emacs/etc/private.el
@@ -0,0 +1,4 @@
+(setq libera-password "***REMOVED***")
+(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+(add-to-list 'default-frame-alist '(ns-appearance . dark)) 
+(setq frame-title-format "")
