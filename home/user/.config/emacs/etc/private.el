611e95481a09de4bf267b597a1b6f2e3a59c9c62
MCotocel <MCotocel@outlook.com>
2021-06-15 07:27:18 +0400

    Updated emacs lisp

--- home/user/.config/emacs/etc/private.el
+++ home/user/.config/emacs/etc/private.el
@@ -1,4 +1,4 @@
-(setq libera-password "***REMOVED***")
+(setq libera-password "YOURPASSWORDHERE")
 (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
 (add-to-list 'default-frame-alist '(ns-appearance . dark)) 
 (setq frame-title-format "")
