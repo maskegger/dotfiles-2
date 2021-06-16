ad545f7ecc77a3488b789792cd8886327071ac77
MCotocel <MCotocel@outlook.com>
2021-06-16 09:17:47 +0400

    Config overhaul

--- home/user/.config/emacs/etc/private.el
+++
@@ -1,4 +0,0 @@
-(setq libera-password "YOURPASSWORDHERE")
-(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
-(add-to-list 'default-frame-alist '(ns-appearance . dark)) 
-(setq frame-title-format "")
